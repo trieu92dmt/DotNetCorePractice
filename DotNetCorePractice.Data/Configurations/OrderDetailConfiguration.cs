04a28c2c9ade903e4b80cad27c21ec140e45ce37
trieu92dmt <trieu251101@gmail.com>
2022-09-14 11:54:41 +0700

    create database design

---
+++ DotNetCorePractice.Data/Configurations/OrderDetailConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using DotNetCorePractice.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCorePractice.Data.Configurations
+{
+    public class OrderDetailConfiguration : IEntityTypeConfiguration<OrderDetail>
+    {
+        public void Configure(EntityTypeBuilder<OrderDetail> builder)
+        {
+            builder.ToTable("OrderDetails");
+            builder.HasKey(x => new { x.OrderId, x.ProductId });
+            builder.HasOne(x => x.Order).WithMany(pc => pc.OrderDetails).HasForeignKey(x => x.OrderId);
+            builder.HasOne(x => x.Product).WithMany(pc => pc.OrderDetails).HasForeignKey(x => x.ProductId);
+        }
+    }
+}
